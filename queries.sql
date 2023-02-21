2e72c36230589e1420597cc9c4ed02823e11a24c
Teklay <teklay@gmail.com>
2023-02-21 17:29:43 +0300

    Add solutions for given queries

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
-/*Queries that provide answers to the questions from all projects.*/
+/Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM `animals` WHERE name LIKE '%mon%';
 SELECT name FROM `animals` WHERE date_of_birth BETWEEN '2016-01-01' and '2019-01-01';
@@ -8,3 +8,23 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM `animals` WHERE neutered = 1;
 SELECT * FROM `animals` WHERE name != 'Gabumon';
 SELECT * FROM `animals` WHERE weight_kg>=10.4 OR weight_kg<=17.3 AND weight_kg BETWEEN 10.4 AND 17.3;
+
+Update animals SET species = "unspecified";
+ROLLBACK;
+Update animals SET species = "digimon" where name LIKE "%mon%";
+ Update animals SET species = "pokemon" where name NOT LIKE "%mon%";
+ delete from animals;
+ ROLLBACK;
+delete from animals where date_of_birth > '2022-01-01';
+SAVEPOINT del;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO del;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+SELECT count(name) from animals;
+SELECT count(name) from animals where escape_attempts = 0;
+SELECT avg(weight_kg) from animals;
+SELECT neutered, SUM(escape_attempts) from animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) from animals GROUP BY species;
+SELECT species, AVG(escape_attempts) from animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species;
