8ac3de056b477e2eeeb019b40db65f80debb75dd
Teklay <teklay@gmail.com>
2023-02-21 22:14:44 +0300

    Create new tables and add constraint key

--- schema.sql
+++ schema.sql
@@ -13,3 +13,10 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species varchar(255) AFTER weight_kg; 
+
+CREATE TABLE owners (id int PRIMARY KEY AUTO_INCREMENT, full_name varchar(255), age int);
+CREATE TABLE species (id int PRIMARY KEY AUTO_INCREMENT, name varchar(255));
+ALTER TABLE animals MODIFY id int PRIMARY KEY AUTO_INCREMENT;
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id int, ADD CONSTRAINT FOREIGN KEY(species_id) REFERENCES species(id);
+ALTER TABLE animals ADD owner_id int, ADD FOREIGN KEY(owner_id) REFERENCES owners(id);
